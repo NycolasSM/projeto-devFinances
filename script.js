9173ae39d1997dc4a306bbc7ad904e87242cf11b
Nycolas Santana <nycolassantana00@gmail.com>
2021-02-16 18:29:22 -0300

    versão 1.01 mouse:hover no botao remover transação

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img onclick="Transaction.remove(${index})" src="/assets/minus.svg" alt="Remover Transação">
+                <img class="button-remove" onclick="Transaction.remove(${index})" src="/assets/minus.svg" alt="Remover Transação">
             </td>
         `
         return html
